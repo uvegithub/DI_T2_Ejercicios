1d224cf8beb0e1010917dd86e4ec79b393170c81
Vanesa <vanesanunez90dos@gmail.com>
2023-11-21 18:48:34 +0100

    actu 21.11

---
+++ app/src/main/java/com/example/di_t2_ejer1/Ejercicio3_T2.kt
@@ -0,0 +1,11 @@
+package com.example.di_t2_ejer1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio3_T2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio3_t2)
+    }
+}
